cd694b9000f9c6c3094a39fbb78801ad608b901e
Samantha White <samanthawhite@fb.com>
2018-07-02 11:37:52 -0700

    Sign In success

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -3,7 +3,7 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Context;
 
 import com.codepath.oauth.OAuthBaseClient;
-import com.github.scribejava.apis.FlickrApi;
+import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.api.BaseApi;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -21,10 +21,10 @@ import com.loopj.android.http.RequestParams;
  *
  */
 public class TwitterClient extends OAuthBaseClient {
-    public static final BaseApi REST_API_INSTANCE = FlickrApi.instance(FlickrApi.FlickrPerm.WRITE); // Change this
-    public static final String REST_URL = "https://api.flickr.com/services"; // Change this, base API URL
-    public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
-    public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
+    public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();
+    public static final String REST_URL = "https://api.twitter.com/1.1";
+    public static final String REST_CONSUMER_KEY = "1HnQS2eBEOmnI51l9VzPL6fEj";       // Change this
+    public static final String REST_CONSUMER_SECRET = "EyEmviJScyuKxMfkXZPJCKND2NoZGjpsp3qavshzXNjNr9E5h3"; // Change this
 
     // Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
     public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
