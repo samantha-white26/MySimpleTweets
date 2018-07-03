25ed8774c5541086819d891a2fe4ca2de0dcfeb9
Samantha White <samanthawhite@fb.com>
2018-07-02 19:27:21 -0700

    End of day one

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,12 +2,18 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -16,54 +22,92 @@ public class TimelineActivity extends AppCompatActivity {
     //reference to twitter client
     private TwitterClient client;
 
+    //we're going to connect the adapter here
+
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-
-
-
         //get access to twitter client
         client = TwitterApplication.getRestClient(this);
+
+        //find the recyclerView
+        rvTweets = (RecyclerView) findViewById(R.id.rvTweet);
+        //instantiate the arraylist (data source)
+        tweets = new ArrayList<>();
+        //construct the adapter from this datasource (what does this mean)
+        tweetAdapter = new TweetAdapter(tweets);
+
+        //RecyclerView Setup(Layout manager, set the recycler view to use the
+        //adapter... connect the recyclerview to the adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        //set the adapter
+        rvTweets.setAdapter(tweetAdapter);
+
         populateTimeline();
+
+
     }
 
     private void populateTimeline() {
     // make network request to get info from twitter api and make an anonymous class to deal with
     // response
-    client.getHomeTimeline(new JsonHttpResponseHandler() {
-
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-            Log.d("TwitterClient", response.toString());
-        }
-
-
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-            Log.d("TwitterClient", response.toString());
-        }
-
-
+    client.getHomeTimeline(
+        new JsonHttpResponseHandler() {
 
           @Override
-          public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+          public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            Log.d("TwitterClient", response.toString());
+          }
+
+          @Override
+          public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+            //   Log.d("TwitterClient", response.toString());
+
+            // iterate through the JSON array
+            // for each entry deserialize the JSON object
+            for (int i = 0; i < response.length(); i++) {
+              // convert each object to a tweet model
+                Tweet tweet = null;
+                try {
+                    tweet = Tweet.fromJSON(response.getJSONObject(i));
+                    // add that tweet model to our data source
+                    tweets.add(tweet);
+                    // notify the adapter that an
+                    // item was added
+                    tweetAdapter.notifyItemInserted(tweets.size()-1);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+          }
+
+          @Override
+          public void onFailure(
+              int statusCode, Header[] headers, String responseString, Throwable throwable) {
             Log.d("TwitterClient", responseString);
             throwable.printStackTrace();
           }
 
           @Override
-          public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+          public void onFailure(
+              int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
             Log.d("TwitterClient", errorResponse.toString());
             throwable.printStackTrace();
           }
 
           @Override
-          public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+          public void onFailure(
+              int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
             Log.d("TwitterClient", errorResponse.toString());
             throwable.printStackTrace();
           }
         });
-
     }
 }
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,104 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+
+    private List<Tweet> mTweets;
+    Context context;
+
+    //pass in the tweets array into the construction
+    //declare our constructor
+    //This is confusing to me
+    public TweetAdapter(List<Tweet> tweets){
+        mTweets = tweets;
+    }
+
+    //for each row, inflate the layout and pass into view holder class
+
+
+    //invoked/created when need to make a new row
+    //in other cases as user scrolls down the user, on bindview holder gets called
+    //inflates the layout and stores the findbyid viewholders
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflate the layout that we created
+        //get context
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+
+        //inflate the tweet row
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+
+    }
+
+    //bind the values based on the position of the element... position used to look up which tweet to
+    //look up and populate
+    //Where does this position come from?
+    //passes the position of a previously cached viewholder and repopulates
+    //based on the position... but again what is this position?
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //which tweet object to show based on position
+        Tweet tweet = mTweets.get(position);
+        Log.i("awesometest", "position:" + position);
+
+        //populate the views according to the data above
+        //right now just textview
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+
+        //glide to put in the image
+        GlideApp.with(context).load(tweet.user.profileImgUrl).into(holder.ivProfileImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+//create viewholder class
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        //the views I want to display
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+
+        //constructor takes in inflated layout ... What is an inflated layout??
+        public ViewHolder (View itemView){
+            super(itemView);
+
+            //perform findbiewby id lookups
+
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUserName);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+
+
+        }
+    }
+}
